1430d5f3914aa86d83cff4e76c9281d03a0d8280
VoidxRaunak <raunakprajapati857@gmail.com>
2026-09-13 22:23:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 io.on("connection", (socket) => {
   console.log("A user connected");
